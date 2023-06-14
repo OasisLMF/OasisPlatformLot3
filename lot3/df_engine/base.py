998752a7a8dbcd55ca5025f8101c49f521a25007
Dan Bate <dan@wildfish.com>
2023-06-14 14:21:37 +0100

    extracted WrappedBase for more generic uses

--- lot3/df_engine/base.py
+++ lot3/df_engine/base.py
@@ -4,57 +4,13 @@ import numpy as np
 import pandas as pd
 
 
-def wrap_result(res, dataframe_class, series_class):
-    if isinstance(res, Callable):
-        return WrappedIndexAndCallable(res, dataframe_class, series_class)
-    elif isinstance(res, dataframe_class.base):
-        return dataframe_class(res)
-    elif isinstance(res, series_class.base):
-        return series_class(res)
-
-    return res
-
-
-class WrappedIndexAndCallable:
-    def __init__(self, base_object, dataframe_class, series_class):
-        self.base_object = base_object
-        self.dataframe_class = dataframe_class
-        self.series_class = series_class
-
-    def __getitem__(self, item):
-        return wrap_result(self.base_object[item], self.dataframe_class, self.series_class)
-
-    def __setitem__(self, key, value):
-        self.base_object[key] = base_v(value)
-
-    def __call__(self, *args, **kwargs):
-        processed_args = [base_v(arg) for arg in args]
-        processed_kwargs = {k: base_v(v) for k, v in kwargs.items()}
-
-        return wrap_result(
-            self.base_object(*processed_args, **processed_kwargs),
-            self.dataframe_class,
-            self.series_class,
-        )
-
-
-def base_v(v):
-    return getattr(v, "base_object", v)
-
-
-class ResultsWrapperMeta(type):
+class WrappedMeta(type):
     def __getattr__(cls, item):
         res = getattr(cls.base, item)
         return wrap_result(res, cls.dataframe_class, cls.series_class)
 
 
-class ResultWrapper(metaclass=ResultsWrapperMeta):
-    BLANK_VALUES = {np.nan, '', None, pd.NA, pd.NaT}
-
-    def __init__(self, data=None, **kwargs):
-        processed_kwargs = {k: base_v(v) for k, v in kwargs.items()}
-        self.base_object = data if isinstance(data, self.base) else self.base(data=data, **processed_kwargs)
-
+class WrappedBase(metaclass=WrappedMeta):
     def __getattr__(self, item):
         attr = getattr(self.base_object, item)
         return self.wrap_result(attr)
@@ -158,6 +114,52 @@ class ResultWrapper(metaclass=ResultsWrapperMeta):
 
     def wrap_result(self, res):
         return wrap_result(res, self.dataframe_class, self.series_class)
+
+
+def wrap_result(res, dataframe_class, series_class):
+    if isinstance(res, Callable):
+        return WrappedIndexAndCallable(res, dataframe_class, series_class)
+    elif isinstance(res, dataframe_class.base):
+        return dataframe_class(res)
+    elif isinstance(res, series_class.base):
+        return series_class(res)
+
+    return res
+
+
+class WrappedIndexAndCallable:
+    def __init__(self, base_object, dataframe_class, series_class):
+        self.base_object = base_object
+        self.dataframe_class = dataframe_class
+        self.series_class = series_class
+
+    def __getitem__(self, item):
+        return wrap_result(self.base_object[item], self.dataframe_class, self.series_class)
+
+    def __setitem__(self, key, value):
+        self.base_object[key] = base_v(value)
+
+    def __call__(self, *args, **kwargs):
+        processed_args = [base_v(arg) for arg in args]
+        processed_kwargs = {k: base_v(v) for k, v in kwargs.items()}
+
+        return wrap_result(
+            self.base_object(*processed_args, **processed_kwargs),
+            self.dataframe_class,
+            self.series_class,
+        )
+
+
+def base_v(v):
+    return getattr(v, "base_object", v)
+
+
+class ResultWrapper(WrappedBase):
+    BLANK_VALUES = {np.nan, '', None, pd.NA, pd.NaT}
+
+    def __init__(self, data=None, **kwargs):
+        processed_kwargs = {k: base_v(v) for k, v in kwargs.items()}
+        self.base_object = data if isinstance(data, self.base) else self.base(data=data, **processed_kwargs)
 
     @property
     def loc(self):
