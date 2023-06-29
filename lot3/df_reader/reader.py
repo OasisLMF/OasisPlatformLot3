607fb40e9183e9cd7d2056da5ebc010c4af15ac6
James Outterside <james.outterside@gmail.com>
2023-06-29 13:32:49 +0100

    Parquet versions added

--- lot3/df_reader/reader.py
+++ lot3/df_reader/reader.py
@@ -33,10 +33,16 @@ class OasisPandasReader(OasisReader):
     Thin wrapper for pandas, to apply func based filters to dataframes following read_*.
     """
 
-    engine = pd
-
     def read_csv(self, *args, **kwargs):
-        df = self.engine.read_csv(*args, **kwargs)
+        df = pd.read_csv(*args, **kwargs)
+
+        if self.df_filters:
+            df = self.apply_df_filters(df)
+
+        return df
+
+    def read_parquet(self, *args, **kwargs):
+        df = pd.read_parquet(*args, **kwargs)
 
         if self.df_filters:
             df = self.apply_df_filters(df)
@@ -50,8 +56,6 @@ class OasisDaskReader(OasisReader):
 
     read_* returns a computed dask dataframe, i.e a pandas dataframe for use in other packages.
     """
-
-    engine = dd
 
     def apply_sql(self, df, filename_or_buffer):
         try:
@@ -95,7 +99,7 @@ class OasisDaskReader(OasisReader):
         if filename_or_buffer.endswith(".zip"):
             kwargs["compression"] = None
 
-        df = self.engine.read_csv(filename_or_buffer, *args, **dask_safe_kwargs)
+        df = dd.read_csv(filename_or_buffer, *args, **dask_safe_kwargs)
 
         if self.df_filters:
             df = self.apply_df_filters(df)
@@ -108,10 +112,28 @@ class OasisDaskReader(OasisReader):
         # return as a pandas dataframe
         return df.compute()
 
+    def read_parquet(self, path, *args, **kwargs):
+        df = dd.read_parquet(path, *args, **kwargs)
+
+        # Currently categorical queries are not supported in dask https://github.com/dask-contrib/dask-sql/issues/423
+        # When reading csv, these become strings anyway, so for now we convert to strings.
+        category_cols = df.select_dtypes(include="category").columns
+        for col in category_cols:
+            df[col] = df[col].astype(str)
+
+        if self.df_filters:
+            df = self.apply_df_filters(df)
+
+        if self.sql:
+            df = self.apply_sql(df, path)
+            if df is False:
+                return None
+
+        # return as a pandas dataframe
+        return df.compute()
+
 
 class OasisSparkReader(OasisReader):
-    engine = ps
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.spark = SparkSession.builder.appName("OasisPlatformLot3").getOrCreate()
@@ -127,6 +149,7 @@ class OasisSparkReader(OasisReader):
 
     def read_csv(self, filename_or_buffer, *args, **kwargs):
         df = self.spark.read.csv(filename_or_buffer, header=True, inferSchema=True)
+        # TODO handle buffer?
         # TODO when there is no SQL we will get nothing, it might be better to use pyspark.pandas.sql
         # but then we can't combine filter with sql, depends if that is needed.
         if not self.sql:
@@ -144,10 +167,45 @@ class OasisSparkReader(OasisReader):
         # evaluation for now. We need to see how this impacts the other code I can't see any obvious uses
         # of the parse_dates etc in read_csv.
         object_cols = df.select_dtypes(include="object").columns
-        df[object_cols] = df[object_cols].fillna("").astype(str)
+        df[object_cols] = df[object_cols].astype(str)
 
         # standard filters will need to be after conversion back to pandas for spark.
         if self.df_filters:
             df = self.apply_df_filters(df)
 
         return df
+
+    def read_parquet(self, path, *args, **kwargs):
+        if not self.sql:
+            df = ps.read_parquet(path)
+
+            if self.df_filters:
+                df = self.apply_df_filters(df)
+
+            if df.empty:
+                return None
+
+            # the sql has some compat issues with pandas, so we need to do some jumping back
+            # and forth with the timestamp fields.
+            timestamp_to_strings = []
+            for col in df.select_dtypes(include="timestamp_ntz").columns:
+                df[col] = df[col].astype(str)
+                timestamp_to_strings.append(col)
+
+            df = df.to_pandas()
+        else:
+            df = self.spark.read.parquet(path)
+            df = self.apply_sql(df, path)
+            if not df:
+                return None
+
+            # as per timestamp_to_strings except we access the timestamps via dtypes in spark sql.
+            timestamp_to_strings = []
+            for col, col_type in df.dtypes:
+                if col_type in ["timestamp_ntz"]:
+                    df = df.withColumn(col, df[col].cast("string"))
+                    timestamp_to_strings.append(col)
+
+            df = df.toPandas()
+
+        return df.astype({col: "datetime64[ns]" for col in timestamp_to_strings})
---
+++ tests/df_reader/test_read_parquet.py
@@ -0,0 +1,201 @@
+from tempfile import NamedTemporaryFile
+from unittest.mock import patch
+
+import numpy as np
+import pandas as pd
+import pytest
+from pandas._testing import assert_frame_equal
+
+from lot3.df_reader.reader import (OasisDaskReader, OasisPandasReader,
+                                   OasisSparkReader)
+
+
+@pytest.fixture
+def df():
+    return pd.DataFrame(
+        {
+            "A": 1.0,
+            "B": [
+                pd.Timestamp("20230101"),
+                pd.Timestamp("20230102"),
+                pd.Timestamp("20230102"),
+                pd.Timestamp("20230102"),
+            ],
+            "C": pd.Series(1, index=list(range(4)), dtype="float64"),
+            "D": np.array([3] * 4),
+            "E": pd.Categorical(["test", "train", "test", "train"]),
+            "F": "foo",
+        }
+    )
+
+
+@pytest.mark.parametrize(
+    "reader", [OasisPandasReader, OasisDaskReader, OasisSparkReader]
+)
+def test_read_parquet__expected_pandas_dataframe(reader, df):
+    with NamedTemporaryFile(suffix=".parquet") as parquet:
+        df.to_parquet(path=parquet.name, index=False)
+
+        result = reader().read_parquet(parquet.name)
+
+        assert isinstance(result, pd.DataFrame)
+        assert result.to_dict() == {
+            "A": {0: 1.0, 1: 1.0, 2: 1.0, 3: 1.0},
+            "B": {
+                0: pd.Timestamp("20230101"),
+                1: pd.Timestamp("20230102"),
+                2: pd.Timestamp("20230102"),
+                3: pd.Timestamp("20230102"),
+            },
+            "C": {0: 1.0, 1: 1.0, 2: 1.0, 3: 1.0},
+            "D": {0: 3, 1: 3, 2: 3, 3: 3},
+            "E": {0: "test", 1: "train", 2: "test", 3: "train"},
+            "F": {0: "foo", 1: "foo", 2: "foo", 3: "foo"},
+        }
+
+
+@pytest.mark.parametrize(
+    "reader", [OasisPandasReader, OasisDaskReader, OasisSparkReader]
+)
+def test_read_parquet__df_filter__expected_pandas_dataframe(reader, df):
+    with NamedTemporaryFile(suffix=".parquet") as parquet:
+        df.to_parquet(path=parquet.name, index=False)
+
+        def sample_filter(filter_df):
+            return filter_df[filter_df["E"] == "test"]
+
+        result = reader(df_filters=[sample_filter]).read_parquet(parquet.name)
+
+        assert isinstance(result, pd.DataFrame)
+        assert result.to_dict() == {
+            "A": {0: 1.0, 2: 1.0},
+            "B": {0: pd.Timestamp("20230101"), 2: pd.Timestamp("20230102")},
+            "C": {0: 1.0, 2: 1.0},
+            "D": {0: 3, 2: 3},
+            "E": {0: "test", 2: "test"},
+            "F": {0: "foo", 2: "foo"},
+        }
+
+
+@pytest.mark.parametrize(
+    "reader", [OasisPandasReader, OasisDaskReader, OasisSparkReader]
+)
+def test_read_parquet__df_filter__multiple__expected_pandas_dataframe(reader, df):
+    with NamedTemporaryFile(suffix=".parquet") as parquet:
+        df.to_parquet(path=parquet.name, index=False)
+
+        result = reader(
+            df_filters=[
+                lambda x: x[x["E"] == "test"],
+                lambda x: x[x["B"] == "2023-01-02"],
+            ]
+        ).read_parquet(parquet.name)
+
+        assert isinstance(result, pd.DataFrame)
+        assert result.to_dict() == {
+            "A": {2: 1.0},
+            "B": {2: pd.Timestamp("20230102")},
+            "C": {2: 1.0},
+            "D": {2: 3},
+            "E": {2: "test"},
+            "F": {2: "foo"},
+        }
+
+
+def test_read_parquet__dask__sql__expected_pandas_dataframe(df):
+    with NamedTemporaryFile(suffix=".parquet") as parquet:
+        df.to_parquet(path=parquet.name, index=False)
+
+        result = OasisDaskReader(
+            sql="SELECT * FROM table WHERE E = 'test' AND B = '2023-01-02'"
+        ).read_parquet(parquet.name)
+
+        assert isinstance(result, pd.DataFrame)
+        assert result.to_dict() == {
+            "A": {2: 1.0},
+            "B": {2: pd.Timestamp("20230102")},
+            "C": {2: 1.0},
+            "D": {2: 3},
+            "E": {2: "test"},
+            "F": {2: "foo"},
+        }
+
+
+def test_read_parquet__dask__sql__invalid_sql(df, caplog):
+    with NamedTemporaryFile(suffix=".parquet") as parquet:
+        df.to_parquet(path=parquet.name, index=False)
+
+        result = OasisDaskReader(sql="SELECT X FROM table").read_parquet(parquet.name)
+
+        assert not result
+        assert caplog.messages == ["Invalid SQL provided"]
+
+
+def test_read_parquet__dask__sql__no_data(df, caplog):
+    with NamedTemporaryFile(suffix=".parquet") as parquet:
+        df.to_parquet(path=parquet.name, index=False)
+
+        result = OasisDaskReader(
+            sql="SELECT * FROM table WHERE E = 'tester'"
+        ).read_parquet(parquet.name)
+
+        assert isinstance(result, pd.DataFrame)
+        assert result.to_dict() == {
+            "A": {},
+            "B": {},
+            "C": {},
+            "D": {},
+            "E": {},
+            "F": {},
+        }
+
+
+def test_read_parquet__spark__sql__expected_pandas_dataframe(df):
+    with NamedTemporaryFile(suffix=".parquet") as parquet:
+        df.to_parquet(path=parquet.name, index=False)
+
+        result = OasisSparkReader(
+            sql="SELECT * FROM table WHERE E = 'test' AND B = '2023-01-02'"
+        ).read_parquet(parquet.name)
+
+        assert isinstance(result, pd.DataFrame)
+
+        # spark has no concept of indexing, so it will essentially drop it hence, 2 being 0 below
+        # if we do need to retain indexing we will need to add it as column prior to sql
+        assert result.to_dict() == {
+            "A": {0: 1.0},
+            "B": {0: pd.Timestamp("20230102")},
+            "C": {0: 1.0},
+            "D": {0: 3},
+            "E": {0: "test"},
+            "F": {0: "foo"},
+        }
+
+
+def test_read_parquet__spark__sql__invalid_sql(df, caplog):
+    with NamedTemporaryFile(suffix=".parquet") as parquet:
+        df.to_parquet(path=parquet.name, index=False)
+
+        result = OasisSparkReader(sql="SELECT X FROM table").read_parquet(parquet.name)
+
+        assert not result
+        assert caplog.messages == ["Invalid SQL provided"]
+
+
+def test_read_parquet__spark__sql__no_data(df, caplog):
+    with NamedTemporaryFile(suffix=".parquet") as parquet:
+        df.to_parquet(path=parquet.name, index=False)
+
+        result = OasisSparkReader(
+            sql="SELECT * FROM table WHERE E = 'tester'"
+        ).read_parquet(parquet.name)
+
+        assert isinstance(result, pd.DataFrame)
+        assert result.to_dict() == {
+            "A": {},
+            "B": {},
+            "C": {},
+            "D": {},
+            "E": {},
+            "F": {},
+        }
